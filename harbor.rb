d9116df06e26bf66f062cbd6a76d2cd09e1566cb
Niklas Forst <>
2025-12-16 12:29:38 +0100

    Add harbor formula

---
+++ harbor.rb
@@ -0,0 +1,32 @@
+class Harbor < Formula
+  desc "A lightweight macOS local dev tool that runs web projects without VMs or containers. It auto-sets local domains and trusted HTTPS, and can proxy services to custom URLs."
+  homepage "https://github.com/nforst/harbor"
+  url "https://github.com/nforst/harbor/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "52a0bed5fc6c453117cc1b08367d9f239ba4cb55bfb8aa53bb46208bcd9236f7"
+  license "ISC"
+
+  depends_on "node"
+
+  def install
+    system "npm", "ci"
+    system "npm", "run", "build"
+    system "chmod", "+x", "dist/index.js"
+
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"dist/index.js" => "harbor"
+  end
+
+  def caveats
+    <<~EOS
+      To complete the installation, run:
+        harbor install
+
+      Before uninstalling, run:
+        harbor uninstall
+    EOS
+  end
+
+  test do
+    system "#{bin}/harbor", "--help"
+  end
+end
