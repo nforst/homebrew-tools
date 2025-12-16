c5f78aab74b451ae2ffa6517eac3d8266810dcb0
Niklas Forst <>
2025-12-16 16:59:52 +0100

    Update harbor to v1.0.10

--- harbor.rb
+++ harbor.rb
@@ -1,8 +1,8 @@
 class Harbor < Formula
   desc "A lightweight macOS local dev tool that runs web projects without VMs or containers. It auto-sets local domains and trusted HTTPS, and can proxy services to custom URLs."
   homepage "https://github.com/nforst/harbor"
-  url "https://github.com/nforst/harbor/archive/refs/tags/v1.0.9.tar.gz"
-  sha256 "0e01f70b1a8a56da99c04fc288725f13c0a09dfeac7ccf1241cfb9812222c2f5"
+  url "https://github.com/nforst/harbor/archive/refs/tags/v1.0.10.tar.gz"
+  sha256 "2f9e0140e556bcf4c9bcb668e187530c13ff27082ed4fe46f1edab6fe8571156"
   license "ISC"
 
   depends_on "node"
